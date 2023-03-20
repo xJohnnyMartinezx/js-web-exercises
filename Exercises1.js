66c43d9eece3aee41b1b229e603351e66cbd34fc
Johnny Martinez <mrtnz.jn@gmail.com>
2023-03-20 11:21:19 -0500

    completed Operators exercises

--- Exercises1.js
+++ Exercises1.js
@@ -1,4 +1,17 @@
-console.log(typeof 99.9)
-console.log(typeof "false")
-console.log(typeof false)
-console.log(typeof '0')
+// Primitive Types Exercises
+console.log(typeof 99.9);
+console.log(typeof "false");
+console.log(typeof false);
+console.log(typeof '0');
+
+//Operators Exercises
+console.log('1' + 2);
+console.log(typeof '1' + 2);
+console.log(6 % 4);
+console.log('3 + 4 is ' + 3 + 4);
+// how could you make this produce the correct output?
+console.log('3 + 4 is ' + (3 + 4));
+console.log(0 < 0);
+console.log('false' == false);
+
+
