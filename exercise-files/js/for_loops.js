98ba46ffa7bb301a9231e745379125de530a8b23
Johnny <mrtnz.jn@gmail.com>
2023-03-24 18:39:39 -0500

    completed exercise 3

--- exercise-files/js/for_loops.js
+++ exercise-files/js/for_loops.js
@@ -3,16 +3,48 @@
 // IIFE Function
 (function (){
 
-    function showMultiplicationTables(num){
+    // **** FOR LOOPS ****
 
-        for (var i = 1; i <= 10; i++){
-            console.log(num + " x " + i + " = " + (num * i));
+    // EXERCISES 1 AND 2
+
+//     function showMultiplicationTables(num){
+//
+//         for (var i = 1; i <= 10; i++){
+//             console.log(num + " x " + i + " = " + (num * i));
+//         }
+//
+//     }
+//
+//     console.log("vvv Exercise 1 and 2")
+//     showMultiplicationTables(7);
+//
+// //      EXERCISE 3
+//
+// //     FOR LOOP
+//     console.log("vvv Exercise 3");
+//         // THE FOR LOOP.
+//         for (var j = 0; j <= 10; j++) {
+//             // GENERATES A RANDOM NUMBER BETWEEN 20 AND 200 (INCLUSIVE)
+//             // HAS TO BE INSIDE THE "FOR LOOP" IN ORDER FOR IT TO BE LOOPED THROUGH.
+//             var min = Math.ceil(20);
+//             var max = Math.floor(200);
+//             var randomNum = Math.floor(Math.random() * (max - min + 1) + min);
+//                 // IF STATEMENT CHECKS IF randomNum IS ODD OR EVEN AND PRINTS THE RESULTS.
+//                 if (randomNum % 2 !== 0) {
+//                     console.log(randomNum + " is odd.");
+//                 } else if (randomNum % 2 === 0) {
+//                     console.log(randomNum + " is even.");
+//                 }
+//         }
+
+
+//  EXERCISE 4
+
+    console.log("vvv Exercise 4");
+
+    for (var x = 1; x <=9; x++){
+        console.log();
         }
-
-    }
-
-    showMultiplicationTables(7);
-
 
 
 
