f50d2543bee11875e27dceb35968b6fd44ce47fe
Johnny Martinez <mrtnz.jn@gmail.com>
2023-03-27 10:52:16 -0500

    completed jquery selector exercises

--- exercise-files/js/jquery_ex.js
+++ exercise-files/js/jquery_ex.js
@@ -4,6 +4,25 @@ $(function() {
     // alert('The DOM has finished loading!');
     // console.log("test");
 
-    var contents = $(".list").html();
-    alert(contents);
+    // #3
+    // var contents = $("#para").html();
+    // alert(contents);
+
+//     #4
+//     var contents = $("#item1").html();
+//     alert(contents);
+
+//     CLASS SELECTORS
+    $(".codeup").css('border', '1px solid #FF0000');
+
+//     TAG SELECTORS
+//     $("li").css('font-size', '20px').css( 'background-color', '#FFFF00');
+//
+//     $("p").css("background-color",  "#FFFF00");
+//     $("h1").css("background-color", "#FFFF00");
+
+//     COMBINED SELECTORS
+    $("li, p, h1").css("background-color", "#2fff00");
+
+
 });
