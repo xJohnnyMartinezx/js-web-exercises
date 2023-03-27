6907416ad87af6fe11b5342c6b02cbb29fd9076d
Johnny Martinez <mrtnz.jn@gmail.com>
2023-03-27 11:58:26 -0500

    added css files

--- exercise-files/js/jquery_ex.js
+++ exercise-files/js/jquery_ex.js
@@ -13,7 +13,7 @@ $(function() {
 //     alert(contents);
 
 //     CLASS SELECTORS
-    $(".codeup").css('border', '1px solid #FF0000');
+//     $(".codeup").css('border', '1px solid #FF0000');
 
 //     TAG SELECTORS
 //     $("li").css('font-size', '20px').css( 'background-color', '#FFFF00');
@@ -22,7 +22,41 @@ $(function() {
 //     $("h1").css("background-color", "#FFFF00");
 
 //     COMBINED SELECTORS
-    $("li, p, h1").css("background-color", "#2fff00");
+//     $("li, p, h1").css("background-color", "#2fff00");
+
+
+//     MOUSE EVENTS
+
+    // SINGLE CLICK
+    $('.title').click(function() {
+        $('.title').css("background-color", "#2fff00")
+    });
+
+//     DOUBLE CLICK
+
+    $('p').dblclick(function(e) {
+        $('p').css('font-size', '18px');
+        // this.css('font-size', '18px');
+
+    });
+
+//     MOUSE HOVER
+//     $('li').hover( handlerIn, handlerOut );
+
+    $('li').hover(
+        function() {
+            $(this).css('color', '#ff0000');
+        },
+        function() {
+            $(this).css('color', '#000000');
+        }
+    );
+
+
+
+
+
+
 
 
 });
