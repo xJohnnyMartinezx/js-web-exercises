a6e1420f00e1761b1aa2246f5f27aa258a93038e
Johnny Martinez <mrtnz.jn@gmail.com>
2023-04-10 16:29:42 -0500

    finished promises exercises

--- js/promises.js
+++ js/promises.js
@@ -1,3 +1,13 @@
 "use strict";
-fetch('https://api.github.com/users', {headers: {'Authorization': `Bearer ${GIT_API}`}})
-    .then(response => console.log(response))
+
+function gitUser(username){
+
+fetch(`https://api.github.com/users/${username}/events/public`,
+    {headers: {'Authorization': `Bearer ${GIT_API}`}})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data[0].created_at);
+    })
+}
+
+gitUser("xjohnnymartinezx")
